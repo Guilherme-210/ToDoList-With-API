04ea3eb8357b06d0898cdb0ac146ea73b909f84b
Guilherme Reis <guireis1164@gmail.com>
2025-05-30 11:24:07 -0300

    Feat(ToDo-API): Adds endpoint to get a single task and complements API with data from frontend

--- ToDoList-API/index.js
+++ ToDoList-API/index.js
@@ -9,27 +9,51 @@ let todoList = []
 
 // Listar tarefas
 app.get("/todolist", (req, res) => {
-  res.json(todoList)
+  return res.json(todoList)
+})
+
+// listar uma tarefa
+app.get("/todolist/:id", (req, res) => {
+  const todo = todoList.find((t) => t.id === parseInt(req.params.id))
+  if (!todo) return res.status(404).json({ error: "Tarefa não encontrada" })
+
+  return res.json(todo)
 })
 
 // Adicionar tarefa
 app.post("/todolist", (req, res) => {
-  const { title } = req.body
+  const { comment, completionDate, title } = req.body
   const newTodo = {
     id: Date.now(),
-    title: title,
+    createdAt: new Date().toISOString(),
     completed: false,
+    title: title,
+    comment: comment,
+    completionDate: completionDate,
   }
+  
   todoList.push(newTodo)
-  res.status(201).json(newTodo)
+
+  return res.status(201).json(newTodo)
 })
 
 // Edita a tarefa
-app.put("/todolist/:id", (req, res) => {
+app.put("/todolist/editar/:id", (req, res) => {
   const todo = todoList.find((t) => t.id === parseInt(req.params.id))
+
   if (!todo) return res.status(404).json({ error: "Tarefa não encontrada" })
-  todo.title = req.body.title
-  res.json(todo)
+    
+  const { title, comment, completionDate } = req.body
+
+  if (!title || !comment || !completionDate) {
+    return res.status(400).json({ error: "Campos obrigatórios ausentes" })
+  }
+
+  todo.title = title
+  todo.comment = comment
+  todo.completionDate = completionDate
+
+  return res.json(todo)
 })
 
 // Atualizar tarefa
@@ -37,7 +61,7 @@ app.patch("/todolist/:id", (req, res) => {
   const todo = todoList.find((t) => t.id === parseInt(req.params.id))
   if (!todo) return res.status(404).json({ error: "Tarefa não encontrada" })
   todo.completed = req.body.completed
-  res.json(todo)
+  return res.json(todo)
 })
 
 // Deletar tarefa
@@ -46,7 +70,7 @@ app.delete("/todolist/:id", (req, res) => {
 
   console.log("Task deletada com sucesso")
 
-  return response.status(204).send()
+  return res.status(204).send()
 })
 
 app.listen(3000, () => {
